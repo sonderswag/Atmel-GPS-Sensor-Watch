fada306a12a0d31d3fc2d672acd9135a4d74f137
jonrodriguez1 <jonathrr@usc.edu>
2017-04-13 19:16:12 -0700

    Create final.c
    Work in progress.

---
+++ Final_Code/final.c
@@ -0,0 +1,27 @@
+// This is the combined code, for everything
+// Let's see how synchronization works
+
+// Including libraries from other directories
+#include "../GPS/GPS.h"
+#include "../Serial/serial.h"
+#include "../SPI/SPI_control.h"
+#include "../Screen/Screen.h"
+#include "../RFM/RFM69.h"
+#include "../I2C/I2C.h"
+#include "../Digital_IO/DigitalIo.h"
+
+// AVR libraries
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// C libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	// code should go here
+	
+	return 0;
+}
